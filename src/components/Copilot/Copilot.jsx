bc39d7d26b46d07932d0bdf803fc9075f669b6e9
phebechung <phebechung@gmail.com>
2025-02-14 11:13:01 -0500

    linked to backend

--- src/components/Copilot/Copilot.jsx
+++ src/components/Copilot/Copilot.jsx
@@ -5,8 +5,15 @@ function Copilot() {
       <section className="copilot">
         <h3>Are you ready to fly with Pilot?</h3>
         <div className="copilot-conversation">
-            <form>Talk to pilot...</form>
-            <button>Send</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder="Talk to Pilot..."
+                />
+            </form>
+            <button type="send">Send</button>
         </div>
       </section>
     );
--- src/pages/CoPilotPage/CoPilotPage.jsx
+++ src/pages/CoPilotPage/CoPilotPage.jsx
@@ -1,9 +1,28 @@
-import './CoPilotPage.scss';
-import React from 'react'
+import "./CoPilotPage.scss";
+import React from "react";
+import axios from "axios";
+import Copilot from '../../components/Copilot/Copilot';
 import Robot from '../../components/Robot/Robot';
-
+import { useState, useEffect } from "react";
 
 function CoPilotPage() {
+  const [copilotChat, setCopilotChat] = useState(null);
+  const API_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    const getChat = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/chat-bot`);
+        console.log(data);
+        setCopilotChat(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getChat();
+  }, []);
+  if (!copilotChat) {
+    return <>Return Loading...</>;
+  }
   return (
     <div>
       <Copilot/>
@@ -11,5 +30,4 @@ function CoPilotPage() {
     </div>
   )
 }
-
-export default CoPilotPage
+export default CoPilotPage;
