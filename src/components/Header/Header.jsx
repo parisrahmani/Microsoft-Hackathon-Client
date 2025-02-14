bc4ce82ac872ba1d7bd4e81e90316990c8112edf
Sonal Sood <sonalsood97@gmail.com>
2025-02-13 23:39:43 -0500

    Added logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import NavBar from "../NavBar/NavBar";
+import icon from "../../assets/logo/logo.png";
 import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <p className="header__logo">Co-teacher</p>
+      <div className="header__container">
+        <p className="header__logo">Co-teacher</p>
+        <img src={icon} className="header__img" />
+      </div>
       <NavBar />
     </header>
   );
