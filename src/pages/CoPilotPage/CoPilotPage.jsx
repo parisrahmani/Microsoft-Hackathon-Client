cbf256027e19ecf22ef7eac03d191779959aec8b
radha-rani-n <nradharani1998@gmail.com>
2025-02-14 10:48:58 -0500

    fetch chat data from backend

--- src/pages/CoPilotPage/CoPilotPage.jsx
+++ src/pages/CoPilotPage/CoPilotPage.jsx
@@ -1,12 +1,28 @@
-import './CoPilotPage.scss';
-import React from 'react'
+import "./CoPilotPage.scss";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function CoPilotPage() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [copilotChat, setCopilotChat] = useState(null);
+  const API_URL = import.meta.env.VITE_API_URL;
+  useEffect(() => {
+    const getChat = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/chat-bot`);
+        console.log(data);
+        setCopilotChat(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-export default CoPilotPage
+    getChat();
+  }, []);
+
+  if (!copilotChat) {
+    return <>Return Loading...</>;
+  }
+  return <div>Copilot</div>;
+}
+export default CoPilotPage;
