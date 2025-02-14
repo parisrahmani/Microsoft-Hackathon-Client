9cc94e5e13c62a2b9722e5e26bc1f9fa8ca33bc1
Sonal Sood <sonalsood97@gmail.com>
2025-02-13 23:27:43 -0500

    header component and nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import NavBar from "../NavBar/NavBar";
 import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
       <p className="header__logo">Co-teacher</p>
+      <NavBar />
     </header>
   );
 }
